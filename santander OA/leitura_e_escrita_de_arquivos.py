2c89407d5055c16c1070b656c13fa113b82093c6
rafaelalm-Leal <rafael.prgleal@gmail.com>
2026-02-08 12:54:07 -0300

    Estudo sobre a leitura e escrita de arquivos .txt em python

---
+++ santander OA/leitura_e_escrita_de_arquivos.py
@@ -0,0 +1,11 @@
+"""O programa deve:
+1- Pedir uma frase ao usuário
+2- Salvar essa frase em um arquivo chamado anotacoes.txt
+3- Depois, ler o arquivo e mostrar todas as frases salvas"""
+
+with open("anotacoes.txt", "a") as arquivo:
+    arquivo.write(input("Digite sua anotação: ") + "\n")
+    
+with open("anotacoes.txt", "r") as arquivo:
+    for linha in arquivo:
+        print(linha.strip())
